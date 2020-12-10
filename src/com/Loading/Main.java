d5d1c43b63d9c3aacb4b2650bcd2c4c304451454
Sabina-Nakarmi <sabinanakarm6@gmail.com>
2020-12-10 15:53:38 +0545

    Initial commit

---
+++ src/com/Loading/Main.java
@@ -0,0 +1,11 @@
+package com.Loading;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String[] fruits={"Apple","Banana","Mango","Pears"};
+	for(String fruit:fruits)
+        System.out.println(fruit);
+
+    }
+}
